5e3e3945de6ddbf19cceebf3b2eff64cec23265e
mmlamera <221376321@mycput.ac.za>
2024-09-05 00:17:35 +0200

    restriction added when going to checkout

--- luxury_hair_backend/src/main/java/za/ac/cput/controller/UserLoginController.java
+++ luxury_hair_backend/src/main/java/za/ac/cput/controller/UserLoginController.java
@@ -19,12 +19,9 @@ import java.util.Map;
 @CrossOrigin(origins = "http://localhost:5173")
 
 public class UserLoginController {
-    private static final String CLIENT_ID = "116310698020-3tjps2m44tu1vgl6nh6phvt91l0l1mf8.apps.googleusercontent.com";
-
     @Autowired
     private UserLoginService userLoginService;
 
-    // Create a new AdminLogin
     @PostMapping("/create")
     public UserLogin createUser(@RequestBody UserLogin request) {
         return userLoginService.create(request);
@@ -58,51 +55,5 @@ public class UserLoginController {
     public UserLogin update(@RequestBody UserLogin userLogin){
         return userLoginService.update(userLogin);
     }
-
-
-//    @PostMapping("/google-login")
-//    public ResponseEntity<String> googleLogin(@RequestBody GoogleLoginRequest googleLoginRequest) {
-//        String token = googleLoginRequest.getToken();
-//
-//        try {
-//            // Set up Google Public Key Manager and Token Verifier with GsonFactory
-//            GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(
-//                    new NetHttpTransport(),
-//                    new GsonFactory() // Use GsonFactory instead of JacksonFactory
-//            ).setAudience(Collections.singletonList(CLIENT_ID))
-//                    .build();
-//
-//            // Verify the ID Token
-//            GoogleIdToken idToken = verifier.verify(token);
-//
-//            if (idToken == null) {
-//                return ResponseEntity.status(401).body("Invalid Google token.");
-//            }
-//
-//            GoogleIdToken.Payload payload = idToken.getPayload();
-//            String email = payload.getEmail();
-//
-//            UserLogin user = userLoginService.findByEmail(email);
-//            if (user == null) {
-//                // Optionally create a new user if not found
-//                return ResponseEntity.status(401).body("User not found.");
-//            }
-//
-//            return ResponseEntity.ok("Google login successful!");
-//        } catch (IOException | GeneralSecurityException e) {
-//            return ResponseEntity.status(500).body("Internal server error.");
-//        }
-
-//            try {
-//                boolean isAuthenticated = userLoginService.authenticateUser(request);
-//                if (isAuthenticated) {
-//                    return ResponseEntity.ok("Login successful!");
-//                } else {
-//                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password.");
-//                }
-//            } catch (Exception e) {
-//                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred during login.");
-//            }
-
 
 }
