d4cc0aa61a59bb002585a02a82099614fee28b5d
Hitshika <nancyhitshika@yahoo.com>
2024-09-05 10:05:18 +0200

    Luxury hair updated backend

--- luxury_hair_backend/src/main/java/za/ac/cput/services/ICustomerOrderService.java
+++
@@ -1,9 +0,0 @@
-package za.ac.cput.services;
-
-import za.ac.cput.domain.CustomerOrder;
-
-import java.util.List;
-
-public interface ICustomerOrderService extends IService<CustomerOrder,String> {
-    List<CustomerOrder> getall();
-}
